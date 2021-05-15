4941dbc00cc42229d96daf0e5b72fbdb6b11e22f
GdosReis66 <g.souza.v.reis@gmail.com>
2021-05-15 20:41:57 -0300

    PrimeiroC++

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int V1 ;
+    int V2;
+    int soma;
+    cout <<"-------------------------------" <<endl;
+    cout << "programa para somae 2 valores " << endl;
+    cout <<"-------------------------------" << endl << endl << endl;
+
+    cout << " .Digite o valor 1: ";
+      cin >> V1;
+
+    cout << " .Digite o valor 2: ";
+      cin >> V2;
+    cout << endl;
+      soma = (V1+V2);
+
+     cout << "A soma dos valores foi: " << soma;
+
+    return 0;
+}
+
